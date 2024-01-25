e7a06775c55748d5fc9b90ad3a0e3d3322e418fa
Aghakarim-dev <hemidzade1010@gmail.com>
2024-01-26 00:02:18 +0400

    uploaded some changing in the middle of the night :)

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const shoppingBtn = document.getElementById("shoppingBtn");
 const mainContainer = document.getElementsByClassName("mainContainer");
+const myBag = document.getElementById('myBag')
 
 shoppingBtn.addEventListener("click", () => {
   mainContainer[0].classList.toggle("active");
+  myBag.classList.toggle("active")
+
 });
+
+
